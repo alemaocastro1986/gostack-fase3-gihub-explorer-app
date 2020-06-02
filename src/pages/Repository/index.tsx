26be4eabcf32e24cf4f08f02ec6ad5d20b0d860a
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2020-06-02 11:32:45 -0300

    Created layout and styles on the details page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +12,57 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="githubexplorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="avatar_url"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Inventore, deserunt! Maiores, aperiam nostrum libero eius, numquam
+              architecto vel eveniet magnam nihil eos sit, ratione illo itaque.
+              Cum blanditiis eius quasi?
+            </p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1980</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="dasdasdas">
+          <div>
+            <strong>aasnasndkjandsakjnk</strong>
+            <p>cmsdmcsmcmsdkmsddsmç</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
